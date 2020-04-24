5c8290feb7119bb87bdd2f99f2ac78d74b7e4469
hybrydyzacja <bart.scibura2422@gmail.com>
2020-04-24 23:18:54 +0200

    Add xreasources patch

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,12 +10,12 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*		     fg         bg       */
+	/* [Scheme] = { fg, bg }	      */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#FF00EE", "#110011" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSelHighlight] = { "#CA51BA", "#080010" },
+	[SchemeNormHighlight] = { "#AD7FA8", "#080010" },
+	[SchemeOut] = { "#000000", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
